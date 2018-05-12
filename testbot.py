e3560af38085f258d679d7cf0e495e9f95fa5cf0
Махамбет Искандер Сакенулы <@corp.itstep>
2018-05-12 12:07:43 +0600

    b

--- testbot.py
+++ testbot.py
@@ -1,23 +1,19 @@
 import telebot
 from datetime import datetime
 from telebot import types
+import wikipedia
+import requests
 token = '485606860:AAFC1eSP_LksyJSRHsDK0Z9b49Rt4u_YzEI'
 
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 
-def get_time_now():
-    return datetime.strftime(datetime.now(), '%d.%m.%Y %H:%M:%S')
 
-def write_to_log(who,text):
-    file = open('log.txt','a+',encoding = 'utf-8')
-    file.write(' '+str(who)+' '+ str(text)+'\n')
-    file.close()
+
 
 def start(message):
         bot.send_message(message.chat.id, 'Hi')
-        #write_to_log(message.from_user.id,'Пользователь начал использовать бота')
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         
         fund_word_button = types.KeyboardButton('Узнать слово')
@@ -32,11 +28,42 @@ def start(message):
         found_weather_button = types.KeyboardButton('Узнать погоду')
         keyboard.add(found_weather_button)
         bot.send_message(message.chat.id, 'Variant:', reply_markup=keyboard)
-        bot.register_next_step_handler(message,choice_user)
+        bot.register_next_step_handler(message,choise_user)
+        print(search_word)
 
 def choise_user(message):
     if message.text == 'Узнать слово':
-        write_to_log(message.from_user.id,'Пользатель выбрал "узнать слово"')
+        #write_to_log(message.from_user.id,'Пользатель выбрал "узнать слово"')
         bot.send_message(message.from_user.id,'Введите слово: ')
         bot.register_next_step_handler(message, search_word)
+    elif message.text == 'Перевести слово':
+        bot.send_message(message.from_user.id,'Введите слово: ')
+        bot.register_next_step_handler(message,translate_message)
+    else:
+        bot.register_next_step_handler(message,choise_user)
+
+
+
+
+def translate_message(message):
+    print('gggg')
+
+    data = {'key': 'trnsl.1.1.20180428T041527Z.0bb0a0164e10fffa.105936f712da2811ffb3ddbd8f24d47a104a0ed3',
+            'text': message.text,
+            'lang':'ru-en',
+            'format':'plain'}
+    
+    answer = requests.post('https://translate.yandex.net/api/v1.5/tr.json/translate', data=data).json()
+
+    bot.send_message(message.from_user.id,answer['text'])
+    bot.register_next_step_handler(message,choise_user)
+    
+
+def search_word(message):
+    wikipedia.set_lang('ru')
+    bot.send_message(message.from_user.id,wikipedia.summary(message.text))
+    bot.register_next_step_handler(message,choise_user)
+
+
+
 bot.polling(none_stop=True)
